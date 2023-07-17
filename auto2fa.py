1aab7895254bdc20c1fc288ae4f5b069e805570b
Harisakhtar <109353887+Harisakhtar0@users.noreply.github.com>
2023-07-17 23:14:04 +0500

    Add files via upload

---
+++ auto2fa.py
@@ -0,0 +1,18 @@
+import os,time
+os.system('clear')
+for x in range(20):
+    print('\n Chana ja bosmde ke 😂\n Tera Sath To Prank Ho Gaya ')
+    time.sleep(0.6)
+ 
+██████  ██████   █████  ███    ██ ██   ██ 
+██   ██ ██   ██ ██   ██ ████   ██ ██  ██  
+██████  ██████  ███████ ██ ██  ██ █████   
+██      ██   ██ ██   ██ ██  ██ ██ ██  ██  
+██      ██   ██ ██   ██ ██   ████ ██   ██ 
+                                                                                    ab agaye ho to follow krlo githup                                   
+    
+# File ke size bare krne kelya ye Google se uthaya 😉😂
+time.sleep(4)
+print('\n\n Ye Lazme Parh Ke Jana 😂')
+print ("""\n\n\n 6 har bar sciprts leak nh ki jari kabhi jabhi hasi mzk b hona chya
+Aking ne b ye sc lazmi dehki hogi to usky kiye bharway tool update kr payment di hui ha Kafi logo ne""")
